cb7d257cc34545f420122614031158ab94f2a815
eltonk2k <elieltonpb1@gmail.com>
2021-06-03 22:05:43 -0300

    Teste: Resumo mensal vazio

--- src/main/java/br/pb/curso/teste/ResumoTest.java
+++ src/main/java/br/pb/curso/teste/ResumoTest.java
@@ -1,9 +1,14 @@
 package br.pb.curso.teste;
 
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
 
 import br.pb.curso.core.BaseTeste;
 import br.pb.curso.core.DriverFactory;
@@ -24,11 +29,21 @@ public class ResumoTest extends BaseTeste {
 		Assert.assertEquals("Movimentação removida com sucesso!", resumoPage.obterMensagemSucesso());
 	}
 	
+//	@Test(expected = NoSuchElementException.class)
 	@Test
 	public void test2ResumoMensal() {
 		menuPage.acessarTelaResumo();
 		
 		Assert.assertEquals("Seu Barriga - Extrato", DriverFactory.getDriver().getTitle());
+		
+		List<WebElement> elementosEcontrados = DriverFactory.getDriver().findElements(By.xpath("//*[@id='tabelaExtrato']/tbody/tr"));
+		Assert.assertEquals(0, elementosEcontrados.size());
+		
+//		try {
+//			DriverFactory.getDriver().findElement(By.xpath("//*[@id='tabelaExtrato']/tbody/tr"));
+//			Assert.fail();
+//		} catch (NoSuchElementException e) {
+//		}		
 	}
 
 }
